fa738687728078c3b9c6d2a45c27dfbf8daf7ff1
wookyoungkim <viliketh1s98@naver.com>
2021-05-23 22:00:21 +0900

    주석 추가

--- knn-regression.py
+++ knn-regression.py
@@ -13,33 +13,34 @@ def mae(real_target, pred_target):
 # 1.1 데이터 불러오기
 original_data = pd.read_csv("Video_Games_Sales_as_at_22_Dec_2016.csv")
 
+# 1.2 중요한 column에 대해서 null값 행 아예 제거
 original_data = original_data[original_data["Year_of_Release"].notnull()]
 original_data = original_data[original_data["Genre"].notnull()]
 original_data = original_data[original_data["Publisher"].notnull()]
 original_data = original_data[original_data["Rating"].notnull()]
 
+# 1.3 필요없는 컬럼 제거
 original_data.drop(['Name', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales', 'Developer'], axis=1, inplace=True)
-original_data.info()
 
-# 1.2 NAN값 있는 행 제거
+# 1.4 tbd값 있는 행 제거
 # data_remove = original_data.dropna(axis=0)
 # data_remove = original_data.astype({'User_Score': 'float64'})
 original_data = original_data.replace('tbd', np.NaN)
 
-# # 1.2 Null값의 보정
+# 1.5 Null값의 보정
 imputer_knn = KNNImputer(n_neighbors=5)
 numeric_subset = original_data[["Year_of_Release", "Global_Sales", "Critic_Score", "Critic_Count", "User_Score", "User_Count"]]
 imputed_knn = imputer_knn.fit_transform(numeric_subset)
 imputed_knn = pd.DataFrame(imputed_knn)
 
+# 1.6 원핫 인코딩으로 라벨링
 categorical_subset = original_data[["Platform", "Genre", "Publisher", "Rating"]]
 encoder = ce.one_hot.OneHotEncoder()
 categorical_subset = encoder.fit_transform(categorical_subset)
 categorical_subset = categorical_subset.reset_index()
 features = pd.concat([imputed_knn, categorical_subset], axis = 1)
 
-print(features)
-# 1.3 String인 컬럼 변환
+# 1.7 String인 컬럼 변환
 data_remove = original_data.astype({'User_Score': 'float64'})
 #
 # # 1.4 Platform, Genre, Rating에 대해서 one-hot encoding으로 변환
@@ -62,6 +63,7 @@ target = features[1].values
 # 2.2 test data와 training data 분리
 x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.3, random_state=0)
 
+# 3. knn regression
 regressor = KNeighborsRegressor(7, "distance")
 regressor.fit(x_train, y_train)
 
